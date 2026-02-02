3e5eac2d3c893791a835b15e0058817b83211844
FatemehOstvar <fatemehostvar2005@gmail.com>
2026-02-02 22:23:23 +0330

    logic backbone
    cv generation logic implemented throughout

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import {HomePage} from "./components/showPage"
 import {EnterInfo} from "./components/showPage"
 import {Result} from "./components/showPage"
      export type Pages = "homePage" | "enterInfo" | "result"
+    const preInput = {"name":"Setareh",
+    surname: "O",
+        email:"E@gmail.com",
+         "phone number": +99244243 }
+export type Inputs = typeof preInput
 export function App() {
     const [page, setPage] = useState<Pages>('homePage')
+const [input, setInput] = useState<Inputs>(preInput)
     return (
         <>
             {(page === "homePage") ?
                 (<HomePage setPage={setPage}/>) :
                 (page === "enterInfo") ?
-                    (<EnterInfo setPage={setPage}/>) :
-   (<Result setPage={setPage}/>)
+                    (<EnterInfo setPage={setPage} setInput ={setInput} input = {input}/>) :
+   (<Result setPage={setPage} input={ input}/>)
             }
         </>
     )
--- src/components/showPage.tsx
+++ src/components/showPage.tsx
@@ -1,31 +1,72 @@
 
 import type {Pages} from "../App.tsx";
 import React from "react";
+import type {Inputs} from "../App.tsx";
+type reactPage = React.Dispatch<React.SetStateAction<Pages>>
 type ResultProps= {
-setPage: React.Dispatch<React.SetStateAction<Pages>>
+setPage: reactPage,
+input : Inputs
 }
-function Result({setPage}:ResultProps) {
+type EnterInfoProps={
+    setPage: reactPage,
+        setInput:React.Dispatch<React.SetStateAction<Inputs>>,
+input : Inputs
+}
+function Result({setPage,input}:ResultProps) {
 
     return(
-        <div><p>Welcome To Result</p>
-<button onClick={()=>setPage("enterInfo")}>Edit</button>
+        <div><p>CV</p>
+    <ul>
+        {Object.entries(input).map(([key, val])=>
+            (<li key={key}> {key} : {val} </li>)
+        )}
+    </ul>
+    <button onClick={()=>setPage("enterInfo")}>Edit</button>
+    <button onClick={() => setPage("homePage")}>Home</button>
+
         </div>
-    )
+    );
 }
 
-function EnterInfo({setPage}:ResultProps) {
+function EnterInfo({setPage, setInput, input}:EnterInfoProps) {
 return(
-        <div><p>Welcome To EnterInfo</p>
-<button onClick={()=>setPage("result") }> Generate CV</button>
-        </div>
-    )
+    <div><p>Enter Your Info</p>
+        <div><p>General Info</p>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" defaultValue={input.name}/><br/>
+
+        <label htmlFor="sName">Surname</label>
+        <input type="text" id="sName" defaultValue={input.surname}/><br/>
+
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" defaultValue={input.email}/><br/>
+
+        <label htmlFor="phNumb">Phone Number</label>
+        <input type="number" id="phNumb" defaultValue={input["phone number"]}/><br/>
+        {/*<label htmlFor="phNumb">Phone Number</label>*/}
+        {/*<input type="text" id="phNumb"/><br/>*/}
+</div>
+        <button onClick={() => {
+             setPage("result");
+             setInput({'name': (document.getElementById('name')as HTMLInputElement | null)?.value ?? '',
+    surname:  (document.getElementById('sName')as HTMLInputElement | null)?.value ?? '',
+        email: (document.getElementById('email')as HTMLInputElement | null)?.value ?? '',
+        "phone number": Number((document.getElementById('phNumb')as HTMLInputElement | null)?.value ?? -1) } )
+
+        }}> Generate CV</button>
+        <button onClick={() => setPage("homePage")}>Home</button>
+
+    </div>
+)
 }
 
-function HomePage({setPage}:ResultProps){
+function HomePage({setPage}: {"setPage": reactPage }){
 
     return(
         <div><p>Welcome To CV generator</p>
         <button onClick={()=>setPage("enterInfo")}> new CV </button>
+        <button onClick={() => setPage("homePage")}>Home</button>
+
         </div>
     )
 }
